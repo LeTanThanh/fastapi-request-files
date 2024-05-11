d2e60e7edd750ab5bf79defcad60af0153344d38
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-11 22:17:46 +0700

    Import File

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi import File
+from fastapi import UploadFile
+
+from typing import Annotated
+
+app = FastAPI()
+
+# Import File
+@app.post("/files", response_model = dict[str, object])
+async def read_file_as_bytes(file: Annotated[bytes, File()]):
+  return {"size": len(file)}
+
+@app.post("/upload_files", response_model = dict[str, object])
+async def read_file_as_upload_file(file: Annotated[UploadFile, File()]):
+  filename = file.filename
+  content_type = file.content_type
+  size = file.size
+  content = await file.read()
+  await file.close()
+
+  return {
+    "filename": filename,
+    "content_type": content_type,
+    "size": size,
+    "content": content
+  }
