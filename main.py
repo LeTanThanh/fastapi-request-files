129e31ef92744121be11047e2ed6d379ff12d216
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-11 22:26:45 +0700

    UploadFile with Additional Metadata

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ async def read_file_as_upload_file(file: Annotated[UploadFile, File()]):
 """
 
 # Optional File Upload
+"""
 @app.post("/files", response_model = dict[str, object])
 async def read_file_as_bytes(file: Annotated[bytes | None, File()] = None):
   if not file:
@@ -41,6 +42,27 @@ async def read_file_as_upload_file(file: Annotated[UploadFile | None, File()] = 
   if not file:
     return {"message": "No file"}
 
+  filename = file.filename
+  content_type = file.content_type
+  size = file.size
+  content = await file.read()
+  await file.close()
+
+  return {
+    "filename": filename,
+    "content_type": content_type,
+    "size": size,
+    "content": content
+  }
+"""
+
+# UploadFile with Additional Metadata
+@app.post("/files", response_model = dict[str, object])
+async def read_file_as_bytes(file: Annotated[bytes, File(description = "A file read as bytes")]):
+  return {"size": len(file)}
+
+@app.post("/upload_files", response_model = dict[str, object])
+async def read_file_as_upload_file(file: Annotated[UploadFile, File(description = "A file read as bytes")]):
   filename = file.filename
   content_type = file.content_type
   size = file.size
